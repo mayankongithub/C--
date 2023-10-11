ff5a45073370897e847840940f66aaa18fc27b61
mayank <mayankbansal51351@gmail.com>
2023-10-11 23:28:14 +0530

    commit

--- main6.cpp
+++ main6.cpp
@@ -1,5 +1,14 @@
 #include<iostream>
 using namespace std;
+void swap(int &a,int &b){
+    int temp = a;
+    a = b;
+    b = temp;
+}
 int main(){
-    cout << "hello world";
+    int a = 4;
+    int b = 90;
+    swap(a,b);
+    cout << a << " " << b;
+    cout << "selena";
 }
