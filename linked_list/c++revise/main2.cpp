6c8beb44e3f2743430c7d5fbfd478ddf78cb274f
mayank <mayankbansal51351@gmail.com>
2023-10-18 17:23:25 +0530

    commit

---
+++ linked_list/c++revise/main2.cpp
@@ -0,0 +1,45 @@
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int a;
+//     int b;
+//     cout << "enter value of a";
+//     cin >> a;
+//     cout << "enter value of b";
+//     cin >> b;
+//     if (a == b)
+//     {
+//         cout << "yes";
+//         cout << "hello";
+//     }
+//     else if (a > b)
+//         cout << "yes";
+//     else
+//         cout << "no";
+// }
+
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     int a = 1;
+//     int b = 2;
+//     int c = 3;
+//     if (a > b && a > c){
+//         cout << "a";
+//     }
+        
+//     else if (b > a && b > c){
+//         cout << "b";
+//     }
+//     else{
+//         cout << "c";
+//     }
+// }
+#include<iostream>
+using namespace std;
+int main(){
+    int a = 0;
+    
+}
--- linked_list/insertAtAnyIdx.cpp
+++ linked_list/insertAtAnyIdx.cpp
@@ -1,78 +1,99 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-class node{
+class node
+{
 public:
     int val;
-    node* next;
+    node *next;
 
-    node(int val){
-        this->val=val;
-        this->next=NULL;
+    node(int val)
+    {
+        this->val = val;
+        this->next = NULL;
     }
 };
-class linkedlist{
+class linkedlist
+{
 public:
-    node* head;
-    node* tail;
+    node *head;
+    node *tail;
     int size;
 
-    linkedlist(){
+    linkedlist()
+    {
         this->head = NULL;
         this->tail = NULL;
         this->size = 0;
     }
-    
-    int insertathead(int val){
-        node* temp = new node(val);
-        if(size==0) head = tail = temp;
-        else{
+
+    int insertathead(int val)
+    {
+        node *temp = new node(val);
+        if (size == 0)
+            head = tail = temp;
+        else
+        {
             temp->next = head;
             head = temp;
         }
         size++;
     }
-    int insertattail(int val){
-        node* temp = new node(val);
-        if(size==0) head = tail = temp;
-        else{
+    int insertattail(int val)
+    {
+        node *temp = new node(val);
+        if (size == 0)
+            head = tail = temp;
+        else
+        {
             tail->next = temp;
             tail = temp;
         }
         size++;
     }
-    void insert5(int idx,int val){
-        if(idx<0 || idx>size){
+    void insert5(int idx, int val)
+    {
+        if (idx < 0 || idx > size)
+        {
             cout << "ERROR";
         }
-        else if(idx==0)  insertathead(val);
-        else if(idx==size-1) insertattail(val);
-        else{
-            node* t = new node(val);
-            node* temp = head;
-            for(int i=1;i<=idx-1;i++){
+        else if (idx == 0)
+            insertathead(val);
+        else if (idx == size - 1)
+            insertattail(val);
+        else
+        {
+            node *t = new node(val);
+            node *temp = head;
+            for (int i = 1; i <= idx - 1; i++)
+            {
                 temp = temp->next;
             }
             t->next = temp->next;
             temp->next = t;
-            //return;
+            // return;
         }
     }
-    void display(){
-        node* temp = head;
-        while(temp!=NULL){
+    void display()
+    {
+        node *temp = head;
+        while (temp != NULL)
+        {
             cout << temp->val << " ";
             temp = temp->next;
         }
     }
-    int getvalue(int idx){
-        node* temp = head;
-        for(int i=0;i<idx;i++){
+    int getvalue(int idx)
+    {
+        node *temp = head;
+        for (int i = 0; i < idx; i++)
+        {
             temp = temp->next;
         }
         return temp->val;
     }
 };
-int main(){
+int main()
+{
     linkedlist ll;
     ll.insertathead(40);
     ll.display();
@@ -89,7 +110,7 @@ int main(){
     ll.insertattail(30);
     ll.display();
     cout << endl;
-    ll.insert5(3,10);
+    ll.insert5(3, 10);
     ll.display();
     cout << endl;
     int v = ll.getvalue(2);
