88dcbb7850b92dd49209c104bdebc4bd3abb9cf2
Mayank Sharma <mayank0611sharma@gmail.com>
2023-11-24 23:44:49 +0530

    commit

--- stack/nextGreaterElement.cpp
+++ stack/nextGreaterElement.cpp
@@ -8,14 +8,13 @@ int main(){
     newarr[n-1] = -1;
     stack<int>st;
     st.push(arr[n-1]);
-    for(int i=8-2;i>=0;i--){
+    for(int i=n-2;i>=0;i--){
         while(st.size()>0 && st.top()<=arr[i]){
             st.pop();
         }
         if(st.size()==0) newarr[i] = -1;
         else{
             newarr[i] = st.top();
-            
         }
         st.push(arr[i]);
     }
