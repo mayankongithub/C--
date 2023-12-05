cbc2f4fe6bb63b3bb8935bdf1f6b44227092c2a7
Mayank Sharma <mayank0611sharma@gmail.com>
2023-12-05 18:20:59 +0530

    commit

--- oops/udemy.cpp
+++ oops/udemy.cpp
@@ -56,14 +56,34 @@
 using namespace std;
 class account{
 private:
-    static int balance;
+    int balance;
 public:
-    static void show();
-    
+    void setdata();
+    void getdata();
+    static account& lowbalance(account &a1,account &a2); 
+
 };
-void account :: show(){
+account& account :: lowbalance(account &a1,account &a2){
+    if(a1.balance < a2.balance){
+        return a1;
+    }
+    else{
+        return a2;
+    }
+} 
+
+void account :: getdata(){
     cout << balance;
 }
+void account :: setdata(){
+    cout << "Enter the balance -> ";
+    cin >> balance;
+}
 int main(){
-    account :: show();
+    account a1;
+    a1.setdata();
+    account a2;
+    a2.setdata();
+    account & a3 = account :: lowbalance(a1,a2);
+    a3.getdata();
 }
