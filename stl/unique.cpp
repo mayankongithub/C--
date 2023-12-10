160345ed6c9f63d3b2e0eeb305232dbe6e64d7a2
Mayank Sharma <mayank0611sharma@gmail.com>
2023-12-10 21:29:14 +0530

    hii

--- stl/unique.cpp
+++ stl/unique.cpp
@@ -1,10 +1,15 @@
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     int n = 5;
+//    int arr[n] = {2,2,3,4,5};
+//    int *ptr=unique(arr,arr+n);
+//    for(int i=0;i<ptr-arr;i++){
+//     cout << arr[i] << " ";
+//    }
+// }
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    int n = 5;
-   int arr[n] = {2,2,3,4,5};
-   unique(arr,arr+n);
-   for(int i=0;i<n;i++){
-    cout << arr[i] << " ";
-   }
+   cout << 10/10;
 }
