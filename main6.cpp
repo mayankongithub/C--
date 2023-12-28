3b159942ef4cd46f9ceefdbb2646b479d4ddfe30
Mayank Sharma <mayank0611sharma@gmail.com>
2023-12-28 20:38:34 +0530

    hi

--- main6.cpp
+++ main6.cpp
@@ -838,28 +838,52 @@
 //     }
 // }
 
-#include<iostream>
-#include<stack>
+// #include<iostream>
+// #include<stack>
+// using namespace std;
+// int main(){
+//     int arr[8] = {100,80,60,81,70,60,75,85};
+//     int n = 8;
+//     int newarr[n];
+//     newarr[0] = -1;
+//     stack<int>st;
+//     st.push(0);
+//     for(int i=1;i<n;i++){
+//         while(st.size()>0 && arr[st.top()]<=arr[i]){
+//             st.pop();
+//         }
+//         if(st.size()==0) newarr[i] = -1;
+//         else{
+//             newarr[i] = st.top();
+//         }
+//         st.push(i);
+//     }
+//     for(int i=0;i<n;i++){
+//         newarr[i] = i - newarr[i];
+//         cout << newarr[i] << " ";
+//     }
+// }
+
+#include<bits/stdc++.h>
 using namespace std;
 int main(){
-    int arr[8] = {100,80,60,81,70,60,75,85};
-    int n = 8;
-    int newarr[n];
-    newarr[0] = -1;
+    int arr[] = {10,6,8,5,11,9};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    vector<int>newarr(n,0);
     stack<int>st;
-    st.push(0);
-    for(int i=1;i<n;i++){
-        while(st.size()>0 && arr[st.top()]<=arr[i]){
+    st.push(arr[n-1]);
+    for(int i=n-2;i>=0;i--){
+        int count = 0;
+        while(st.size()>0 && st.top()<arr[i]){
             st.pop();
+            count++;
         }
-        if(st.size()==0) newarr[i] = -1;
-        else{
-            newarr[i] = st.top();
-        }
-        st.push(i);
+        if(st.size()!=0) count++;
+        newarr[i] = count;
+        st.push(arr[i]);
     }
     for(int i=0;i<n;i++){
-        newarr[i] = i - newarr[i];
         cout << newarr[i] << " ";
     }
+    cout << endl;
 }
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,2 +1,58 @@
-default : cout << "Enter the valid operator";
-        //           break;
+// int main(){
+//     string s = "aaaabbbccaaaccdddd";
+//     cout << remove(s);
+// }
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     int arr[8] = {3,1,2,7,4,6,2,3};
+//     int n = 8;
+//     int newarr[n];
+//     newarr[n-1] = -1;
+//     stack<int>st;
+//     st.push(arr[n-1]);
+//     for(int i=n-2;i>=0;i--){
+//         while(st.size()>0 && st.top()<=arr[i]){
+//             st.pop();
+//         }
+//         if(st.size()==0) newarr[i]=-1;
+//         else{
+//             newarr[i] = st.top();
+//         }
+//         st.push(arr[i]);
+//     }
+//     for(int i=0;i<n;i++){
+//         cout << newarr[i] << " ";
+//     }
+// }
+
+// #include<iostream>
+// #include<stack>
+// using namespace std;
+// int main(){
+//     int arr[8] = {100,80,60,81,70,60,75,85};
+//     int n = 8;
+//     int newarr[n];
+//     newarr[0] = -1;
+//     stack<int>st;
+//     st.push(0);
+//     for(int i=1;i<n;i++){
+//         while(st.size()>0 && arr[st.top()]<=arr[i]){
+//             st.pop();
+//         }
+//         if(st.size()==0) newarr[i] = -1;
+//         else{
+//             newarr[i] = st.top();
+//         }
+//         st.push(i);
+//     }
+//     for(int i=0;i<n;i++){
+//         newarr[i] = i - newarr[i];
+//         cout << newarr[i] << " ";
+//     }
+// }
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int arr[] = {10,6,8,5,11,9};
