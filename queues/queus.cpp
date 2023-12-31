3cdc649c7e510d8a5f0f4bb1361459b3c2060d8e
Mayank Sharma <mayank0611sharma@gmail.com>
2023-12-31 23:52:29 +0530

    hi

--- queues/queus.cpp
+++ queues/queus.cpp
@@ -67,16 +67,16 @@
 //     display(q);
 // }
 
-#include<bits/stdc++.h>
+#include<iostream>
 using namespace std;
 class Q{
 public:
     int f;
     int b;
-    int arr;
+    int arr[5];
     Q(){
         f = 0;
-        this->b = 0;
+        b = 0;
     }
     void push(int val){
         if(b==5){
@@ -95,23 +95,29 @@ public:
             f++;
         }
     }
-    void front(){
+    int front(){
         if(f-b==0){
             cout << "Queue is empty!!";
-            return;
+            return 0;
         }
         else{
-            
+            return arr[f];
+        }
+    }
+    void display(){
+        for(int i=f;i<b;i++){
+            cout << arr[i] << " ";
         }
     }
 };
 int main(){
     Q q;
     q.push(10);
-    q.push(10);
-    q.push(10);
-    q.push(10);
+    q.push(20);
+    q.push(30);
+    q.push(40);
     q.display();
-    q.push(10);
+    cout << endl;
+    q.push(50);
     q.display();
 }
