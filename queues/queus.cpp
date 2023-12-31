2d564a006791e03cbf6c01eab25ee0245b93f46d
Mayank Sharma <mayank0611sharma@gmail.com>
2023-12-31 22:17:38 +0530

    hi

---
+++ queues/queus.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+void display(queue<int> &q){
+    int n = q.size();
+    for(int i=0;i<n;i++){
+        int x = q.front();
+        cout << x << " ";
+        q.pop();
+        q.push(x);
+    }
+}
+void reverse(queue<int> &q){
+    stack<int>st;
+    while(q.size()!=0){
+        int x = q.front();
+        q.pop();
+        st.push(x);
+    }
+    while(st.size()!=0){
+        int x = st.top();
+        st.pop();
+        q.push(x);
+    }
+}
+int main(){
+    queue<int>q;
+    q.push(10);
+    q.push(20);
+    q.push(30);
+    q.push(40);
+    q.push(50);
+    display(q);
+    cout << endl;
+    reverse(q);
+    display(q);
+}
