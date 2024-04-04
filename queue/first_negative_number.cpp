9a511b5f3e274a013ee11cf9a69abf42c46fd76d
Mayank Sharma <mayank0611sharma@gmail.com>
2024-04-04 11:44:36 +0530

    queue

--- queue/first_negative_number.cpp
+++ queue/first_negative_number.cpp
@@ -1,6 +1,27 @@
 #include<iostream>
 #include<queue>
+#include<vector>
 using namespace std;
 int main(){
-    
+    int arr[] = {3,-4,-7,30,7,-9,2,1,6,-1};
+    queue<int>q;
+    vector<int>v;
+    for(int i=0;i<10;i++){
+        if(arr[i]<0){
+            q.push(i);
+        }
+    }
+    int k = 3;
+    int i = 0;
+    while(i+k<10){
+        while(q.size()>0 && q.front()<i) q.pop();
+        if(q.size()==0 || q.front()>=i+k)v.push_back(0);
+        else{
+            v.push_back(arr[q.front()]);
+        }
+        i++;
+    }
+    for(int i=0;i<v.size();i++){
+        cout << v.at(i) << " ";
+    }
 }
