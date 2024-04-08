64dd74fb2527966e72ce2eb46f32a004035b7900
Mayank Sharma <mayank0611sharma@gmail.com>
2024-04-08 15:28:34 +0530

    dsa

--- BinaryTree/implemnetation_using_queue.cpp
+++ BinaryTree/implemnetation_using_queue.cpp
@@ -1,102 +1,39 @@
-// #include<iostream>
-// #include<queue>
-// using namespace std;
-// class node{
-// public:
-//     int val;
-//     node* left , *right;
-//     node(int val){
-//         this->val = val;
-//         left = right = NULL;
-//     }
-
-// };
-// int main(){
-//     int x;
-//     cout << "Enter the root value : ";
-//     cin >> x;
-//     queue<node*>q;
-//     node* root = new node(x);
-//     q.push(root);
-//     while(!q.empty()){
-//         node* temp = q.front();
-//         q.pop();
-//         int first;
-//         cout << "Enter the left value of " << temp->val << " : ";
-//         cin >> first;
-//         if(first!=-1){
-//             temp->left = new node(first);
-//             q.push(temp->left);
-//         }
-//         int second;
-//         cout << "Enter the right value of " << temp->val << " : ";
-//         cin >> second;
-//         if(second!=-1){
-//             temp->right = new node(second);
-//             q.push(temp->right);
-//         }
-//     }
-// }
-
 #include<iostream>
 #include<queue>
-#include<vector>
 using namespace std;
-class Node{
+class node{
 public:
     int val;
-    Node* left;
-    Node* right;
-    Node(int val){
+    node* left , *right;
+    node(int val){
         this->val = val;
-        left = NULL;
-        right = NULL;
+        left = right = NULL;
     }
+
 };
-vector<vector<int>> level(Node* root,vector<vector<int>> &ans){
-    if(root==NULL){
-        return ans;
-    }
-    queue<Node*>q;
+int main(){
+    int x;
+    cout << "Enter the root value : ";
+    cin >> x;
+    queue<node*>q;
+    node* root = new node(x);
     q.push(root);
     while(!q.empty()){
-        int size = q.size();
-        vector<int>level;
-        for(int i=0;i<size;i++){
-            Node* node = q.front();
-            q.pop();
-            if(node->left!=NULL)q.push(node->left);
-            if(node->right!=NULL)q.push(node->right);
-            level.push_back(node->val);
+        node* temp = q.front();
+        q.pop();
+        int first;
+        cout << "Enter the left value of " << temp->val << " : ";
+        cin >> first;
+        if(first!=-1){
+            temp->left = new node(first);
+            q.push(temp->left);
         }
-        ans.push_back(level);
-    }
-    return ans;
-}
-int main(){
-    Node* a = new Node(1);
-    Node* b = new Node(2);
-    Node* c = new Node(3);
-    Node* d = new Node(4);
-    Node* e = new Node(5);
-    Node* f = new Node(6);
-    Node* g = new Node(7);
-    Node* h = new Node(8);
-    
-    a->left = b;
-    b->left = d;
-    b->right = e;
-    e->left = f;
-    e->right = g;
-    a->right = c;
-    c->right = h;
-    
-    vector<vector<int>> ans;
-    level(a,ans);
-    for(int i=0;i<ans.size();i++){
-        for(int j=0;j<ans[i].size();j++){
-            cout << ans[i][j] << " ";
+        int second;
+        cout << "Enter the right value of " << temp->val << " : ";
+        cin >> second;
+        if(second!=-1){
+            temp->right = new node(second);
+            q.push(temp->right);
         }
     }
-    //pre(a);
 }
