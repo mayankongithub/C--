495c8c47ef15aaeb95dbe6f017ee32f73a6d4805
Mayank Sharma <mayank0611sharma@gmail.com>
2024-04-12 18:09:29 +0530

    DSA

--- BinaryTree/questions.cpp
+++ BinaryTree/questions.cpp
@@ -264,6 +264,7 @@ void rightboundary(node* root){
     if(root->right==NULL)root->left;
     cout << root->val << " "; 
 }
+
 int main(){
     int arr[] = {1,2,3,4,5,INT_MIN,6,7,INT_MIN,8,INT_MIN,9,10,11,INT_MIN,12,INT_MIN,13,INT_MIN,14,15,16,INT_MIN,17,18,INT_MIN,19,INT_MIN,INT_MIN,INT_MIN,20,21,22,23,INT_MIN,24,25,26,27,INT_MIN,INT_MIN,28,INT_MIN,INT_MIN};
     int n = sizeof(arr)/sizeof(arr[0]);
