4bba55cb79cd28a3015d6e5b48abf177684613c2
Mayank Sharma <mayank0611sharma@gmail.com>
2024-04-16 23:48:43 +0530

    DSA

--- BinaryTree/questions.cpp
+++ BinaryTree/questions.cpp
@@ -377,12 +377,3 @@
 //     //boundary(root);
 //     //rightboundary(root);
 // }
-
-#include<iostream>
-#include<bits/stdc++.h>
-using namespace std;
-int main(){
-    int arr[][4] = {{1,2,3},{4,5,6},{7,8,9},{8,9,0}};
-    int n = sizeof(arr)/sizeof(arr[0]);
-    cout << n;
-}
