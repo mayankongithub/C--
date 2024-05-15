bc24aaf88ac12d7c4774a8789f6d144a13a3fbd5
Mayank Sharma <mayank0611sharma@gmail.com>
2024-05-15 23:20:55 +0530

    DSA

--- BinaryTree/questions.cpp
+++ BinaryTree/questions.cpp
@@ -457,8 +457,10 @@
 // }
 
 #include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
 int main(){
-    cout << 3 * 'abc';
+    string s = "543ahery";
+    sort(s.begin(),s.end());
+    cout << s;
 }
-
