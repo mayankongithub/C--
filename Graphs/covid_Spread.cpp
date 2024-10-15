dfb11993cf75490f5322c9fbe492e5feacfb74fe
mayank <mayank0611sharma@gmail.com>
2024-10-16 00:52:36 +0530

    DSA

---
+++ Graphs/covid_Spread.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int row = 3;
+    int col = 5;
+    int arr[row][col] = {{2,1,0,2,1},{0,0,1,2,1},{1,0,0,2,1}};
+    queue<pair<int,int>>q;
+    for(int i=0;i<row;i++){
+        for(int j=0;j<col;j++){
+            if(arr[i][j]==2){
+                
+            }
+        }
+    }
+}
