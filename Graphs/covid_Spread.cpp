347f0fad8bf56c1acb285a667ee870fe0092dc42
mayank <mayank0611sharma@gmail.com>
2024-10-17 19:13:41 +0530

    DSA

--- Graphs/covid_Spread.cpp
+++ Graphs/covid_Spread.cpp
@@ -1,5 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
+bool valid(int i,int j,int row,int col){
+    return i>=0 && i<row && j>=0 && j<col;
+}
 int main(){
     int row = 3;
     int col = 5;
@@ -8,8 +11,46 @@ int main(){
     for(int i=0;i<row;i++){
         for(int j=0;j<col;j++){
             if(arr[i][j]==2){
-                
+                q.push(make_pair(i,j));
             }
         }
     }
+    int timer = 0;
+    while(!q.empty()){
+        int curr_patient = q.size();
+        while(curr_patient--){
+            int i = q.front().first;
+            int j = q.front().second;
+            q.pop();
+            if(valid(i-1,j,row,col) && arr[i-1][j]==1){
+                arr[i-1][j] = 2;
+                q.push(make_pair(i-1,j));
+            }
+            if(valid(i+1,j,row,col) && arr[i+1][j]==1){
+                arr[i+1][j] = 2;
+                q.push(make_pair(i+1,j));
+            }
+            if(valid(i,j-1,row,col) && arr[i][j-1]==1){
+                arr[i][j-1] = 2;
+                q.push(make_pair(i,j-1));
+            }
+            if(valid(i,j+1,row,col) && arr[i][j+1]==1){
+                arr[i][j+1] = 2;
+                q.push(make_pair(i,j+1));
+            }
+        }
+    }
+    bool flag = false;
+    for(int i=0;i<row;i++){
+        for(int j=0;j<col;j++){
+            if(arr[i][j]==1){
+                flag = true;
+                break;
+            }
+        }
+    }
+    if(flag==true){
+        cout << "no covid completely spread";
+    }
+    else cout << "covid is spreaded";
 }
